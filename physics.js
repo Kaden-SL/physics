8a82f9ce90b3c91a9ea3f5c4dec030320e67bf86
Kaden-SL <126031371+Kaden-SL@users.noreply.github.com>
2023-05-18 17:59:50 -0700

    added background image and fixed index.html

--- physics.js
+++ physics.js
@@ -1,19 +1,29 @@
 class Stage1 extends Phaser.Scene
+
+
+
 {
+    movingPlatform;
+    cursors;
+    platforms;
+    stars;
+    player;
+
     preload(){
         this.load.path = './assets/';
-        this.load.image('mainroom', 'mainroom.png');
-        this.load.image('hallway', 'hallway.png');
-        this.load.image('corrupt', 'corrupt.png');
-        this.load.image('corrupthall', 'corrupthall.png');
-        this.load.audio('humm',['backgroundhmm.mp3']);
-        this.load.audio('door',['doorsound.mp3']);
-        this.load.audio('ding',['ding.mp3']);
-        this.load.audio('anger',['angryhumm.wav']);
+        this.load.image('mars', 'marsground.png');
+        // this.load.image('hallway', 'hallway.png');
+        // this.load.image('corrupt', 'corrupt.png');
+        // this.load.image('corrupthall', 'corrupthall.png');
+        // this.load.audio('humm',['backgroundhmm.mp3']);
+        // this.load.audio('door',['doorsound.mp3']);
+        // this.load.audio('ding',['ding.mp3']);
+        // this.load.audio('anger',['angryhumm.wav']);
 
     }
     create ()
     {
+        this.add.image(960,540, 'mars');
 
     }
     update(){

d9af91c62028914eb5551106f0fdf254d4426a28
Kaden-SL <126031371+Kaden-SL@users.noreply.github.com>
2023-05-18 18:48:33 -0700

    added basic layout for the platforming

--- physics.js
+++ physics.js
@@ -5,14 +5,16 @@ class Stage1 extends Phaser.Scene
 {
     movingPlatform;
     cursors;
+    platform1;
     platforms;
     stars;
-    player;
+    car;
 
     preload(){
         this.load.path = './assets/';
         this.load.image('mars', 'marsground.png');
-        // this.load.image('hallway', 'hallway.png');
+        this.load.image('platform', 'platform.png');
+        this.load.spritesheet('car', 'car.png', { frameWidth: 1920, frameHeight: 1080 });
         // this.load.image('corrupt', 'corrupt.png');
         // this.load.image('corrupthall', 'corrupthall.png');
         // this.load.audio('humm',['backgroundhmm.mp3']);
@@ -24,6 +26,15 @@ class Stage1 extends Phaser.Scene
     create ()
     {
         this.add.image(960,540, 'mars');
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(960, 1300, 'platform').setScale(1.2).refreshBody();
+        this.platform1 = this.physics.add.image(500, 700, 'platform').setScale(.3);
+        this.platform1.setImmovable(true);
+        this.platform1.body.allowGravity = false;
+        this.car = this.physics.add.sprite(100, 450, 'car').setScale(.1);
+        this.car.setBounce(0.2);
+        this.car.setCollideWorldBounds(true);
+
 
     }
     update(){
@@ -146,6 +157,13 @@ const game = new Phaser.Game({
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
         height: 1080
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
     },
     // scene: [Room3,Hallway2],
     scene: [Stage1],
